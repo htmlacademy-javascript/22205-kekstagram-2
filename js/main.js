1b329f32f83bb6ca0509535193df97e52d3a6dfa
PATROL <11875983+xmpatrol@users.noreply.github.com>
2026-09-01 18:58:04 +0300

    Добавляет фильтрацию фотографий с «устранением дребезга»

--- js/main.js
+++ js/main.js
@@ -2,12 +2,15 @@ import { renderThumbnails } from './thumbnails.js';
 import { initUploadForm } from './upload-form.js';
 import { getData } from './api.js';
 import { showDataError } from './message.js';
+import { initFilters } from './filters.js';
+import { debounce } from './util.js';
 
 initUploadForm();
 
 getData()
   .then((photos) => {
     renderThumbnails(photos);
+    initFilters(photos, debounce(renderThumbnails));
   })
   .catch(() => {
     showDataError();
